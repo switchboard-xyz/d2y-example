2c1dd39367bb29e92a1937b41e1a4349435c23cf
Mitch Gildenberg <mitch@switchboard.xyz>
2023-08-30 19:01:06 -0400

    init

--- switchboard-function/build.rs
+++ switchboard-function/build.rs
@@ -1,7 +1,6 @@
 fn main() {
     // Read the environment variable
-    let value =
-        std::env::var("EXAMPLE_PROGRAM").expect("EXAMPLE_PROGRAM must be set");
+    let value = std::env::var("EXAMPLE_PROGRAM").expect("EXAMPLE_PROGRAM must be set");
 
     // Pass it to the Rust compiler
     println!("cargo:rustc-env=EXAMPLE_PROGRAM={}", value);
--- switchboard-function/src/main.rs
+++ switchboard-function/src/main.rs
@@ -1,26 +1,22 @@
-pub mod coinbase;
-use rust_decimal::prelude::*;
-use rust_decimal::Decimal;
-use serde::Deserialize;
-use std::collections::HashMap;
-use switchboard_common;
-use switchboard_evm::sdk::EVMFunctionRunner;
-pub use switchboard_utils::reqwest;
-
 use ethers::{
     prelude::{abigen, SignerMiddleware},
     providers::{Http, Provider},
-    types::I256,
 };
-use rand;
-use rand::seq::SliceRandom;
-use rand::SeedableRng;
-use serde_json::Value;
-use std::hash::Hasher;
-
+use rust_decimal::Decimal;
+use serde::Deserialize;
 use std::sync::Arc;
 use std::time::{Duration, SystemTime};
+use switchboard_evm::sdk::EVMFunctionRunner;
+pub use switchboard_utils::reqwest;
 
+#[derive(Debug, Deserialize)]
+pub struct DeribitRespnseInner {
+    pub mark_iv: Decimal,
+}
+#[derive(Debug, Deserialize)]
+pub struct DeribitResponse {
+    pub result: DeribitRespnseInner,
+}
 
 #[tokio::main(worker_threads = 12)]
 async fn main() {
@@ -38,10 +34,11 @@ async fn main() {
     // set the gas limit and expiration date
     // -- this is the maximum amount of gas that can be used for the transaction (and it's a lot)
     let gas_limit = 5_500_000;
-    let est_time = SystemTime::now()
+    let expiration_time_seconds = SystemTime::now()
         .duration_since(SystemTime::UNIX_EPOCH)
         .unwrap_or(Duration::ZERO)
-        .as_secs();
+        .as_secs()
+        + 120;
 
     // setup the provider + signer
     let provider = Provider::<Http>::try_from("").unwrap();
@@ -51,13 +48,14 @@ async fn main() {
             .unwrap(),
     );
 
-
-    let derebit_response = reqwest::get("https://www.deribit.com/api/v2/public/get_order_book?instrument_name=ETH-29SEP23-2000-C")
-        .await
-        .unwrap()
-        .json()
-        .await
-        .unwrap();
+    let _derebit_response: DeribitResponse = reqwest::get(
+        "https://www.deribit.com/api/v2/public/get_order_book?instrument_name=ETH-29SEP23-2000-C",
+    )
+    .await
+    .unwrap()
+    .json()
+    .await
+    .unwrap();
 
     let contract_address = env!("EXAMPLE_PROGRAM")
         .parse::<ethers::types::Address>()
@@ -66,11 +64,10 @@ async fn main() {
     let receiver_contract = Receiver::new(contract_address, client);
 
     // send the callback to the contract
-    let callback = receiver_contract.callback(0, 0);
+    let callback = receiver_contract.callback(0.into(), 0.into());
 
     // get the calls from the output results
-    let mut callbacks = vec![callback];
-
+    let callbacks = vec![callback];
 
     // Emit the result
     function_runner
@@ -83,12 +80,9 @@ async fn main() {
         .unwrap();
 }
 
-
 #[cfg(test)]
 mod tests {
-    use super::*;
 
     #[tokio::test]
-    async fn test() {
-    }
+    async fn test() {}
 }

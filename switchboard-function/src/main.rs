b77a698e82bd2cb78cf2cf7ee2840bc297f3d5f9
Mitch Gildenberg <mitch@switchboard.xyz>
2023-08-31 09:06:30 -0400

    tidy code

--- switchboard-function/src/main.rs
+++ switchboard-function/src/main.rs
@@ -1,22 +1,22 @@
+use chrono::Utc;
 use ethers::{
     prelude::{abigen, SignerMiddleware},
     providers::{Http, Provider},
+    types::Address,
 };
+use rust_decimal::prelude::FromPrimitive;
 use rust_decimal::Decimal;
 use serde::Deserialize;
-use std::sync::Arc;
-use std::time::{Duration, SystemTime};
 use switchboard_evm::sdk::EVMFunctionRunner;
 pub use switchboard_utils::reqwest;
-use rust_decimal::prelude::FromPrimitive;
 
-
+abigen!(Receiver, r#"[ function callback(int256, uint256) ]"#,);
 static UNUSED_URL: &str = "https://goerli-rollup.arbitrum.io/rpc";
 
 #[derive(Debug, Deserialize)]
 pub struct DeribitRespnseInner {
     pub mark_iv: f64,
-    pub timestamp: u64
+    pub timestamp: u64,
 }
 #[derive(Debug, Deserialize)]
 pub struct DeribitResponse {
@@ -25,26 +25,18 @@ pub struct DeribitResponse {
 
 #[tokio::main(worker_threads = 12)]
 async fn main() {
-    abigen!(
-        Receiver,
-        r#"[
-            function callback(int256, uint256)
-        ]"#,
-    );
+    // --- Initialize clients ---
+    let function_runner = EVMFunctionRunner::new().unwrap();
+    let receiver: Address = env!("EXAMPLE_PROGRAM").parse().unwrap();
+    let client = SignerMiddleware::new_with_provider_chain(
+        Provider::<Http>::try_from(UNUSED_URL).unwrap(),
+        function_runner.enclave_wallet.clone(),
+    )
+    .await
+    .unwrap();
+    let receiver_contract = Receiver::new(receiver, client.into());
 
-
-    // set the gas limit and expiration date
-    // -- this is the maximum amount of gas that can be used for the transaction (and it's a lot)
-    let gas_limit = 5_500_000;
-    let expiration_time_seconds = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .unwrap_or(Duration::ZERO)
-        .as_secs()
-        + 120;
-
-    // setup the provider + signer
-    let provider = Provider::<Http>::try_from(UNUSED_URL).unwrap();
-
+    // --- Logic Below ---
     let derebit_response: DeribitResponse = reqwest::get(
         "https://www.deribit.com/api/v2/public/get_order_book?instrument_name=ETH-29SEP23-2000-C",
     )
@@ -53,35 +45,24 @@ async fn main() {
     .json()
     .await
     .unwrap();
-    println!("{:#?}", derebit_response);
 
-    let contract_address = env!("EXAMPLE_PROGRAM")
-        .parse::<ethers::types::Address>()
-        .unwrap();
-
-
-    let function_runner = EVMFunctionRunner::new().unwrap();
-    let client = Arc::new(
-        SignerMiddleware::new_with_provider_chain(provider, function_runner.enclave_wallet.clone())
-            .await
-            .unwrap(),
-    );
-
-    let receiver_contract = Receiver::new(contract_address, client);
     let mut mark_iv = Decimal::from_f64(derebit_response.result.mark_iv).unwrap();
     mark_iv.rescale(8);
+    println!("{:#?}", derebit_response);
+
     // send the callback to the contract
-    let callback = receiver_contract.callback(mark_iv.mantissa().into(), derebit_response.result.timestamp.into());
+    let callback = receiver_contract.callback(
+        mark_iv.mantissa().into(),
+        derebit_response.result.timestamp.into(),
+    );
 
-    // get the calls from the output results
-    let callbacks = vec![callback];
-
-    // Emit the result
-    function_runner.emit(
-            contract_address,
-            expiration_time_seconds.try_into().unwrap(),
-            gas_limit.into(),
-            callbacks,
+    // --- Emit the result ---
+    function_runner
+        .emit(
+            receiver,
+            (Utc::now().timestamp() + 120).into(),
+            5_500_000.into(),
+            vec![callback],
         )
         .unwrap();
 }

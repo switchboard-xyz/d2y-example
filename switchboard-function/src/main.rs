b9b288246234696fbdfa2ccd6639e82b3b2bb69f
Mitch Gildenberg <mitch@switchboard.xyz>
2023-08-31 09:50:41 -0400

    tidy code

--- switchboard-function/src/main.rs
+++ switchboard-function/src/main.rs
@@ -24,7 +24,7 @@ pub struct DeribitResponse {
 }
 
 #[tokio::main(worker_threads = 12)]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // --- Initialize clients ---
     let function_runner = EVMFunctionRunner::new().unwrap();
     let receiver: Address = env!("EXAMPLE_PROGRAM").parse().unwrap();
@@ -32,19 +32,12 @@ async fn main() {
         Provider::<Http>::try_from(UNUSED_URL).unwrap(),
         function_runner.enclave_wallet.clone(),
     )
-    .await
-    .unwrap();
+    .await?;
     let receiver_contract = Receiver::new(receiver, client.into());
 
     // --- Logic Below ---
-    let derebit_response: DeribitResponse = reqwest::get(
-        "https://www.deribit.com/api/v2/public/get_order_book?instrument_name=ETH-29SEP23-2000-C",
-    )
-    .await
-    .unwrap()
-    .json()
-    .await
-    .unwrap();
+    let url = "https://www.deribit.com/api/v2/public/get_order_book?instrument_name=ETH-29SEP23-2000-C";
+    let derebit_response: DeribitResponse = reqwest::get(url).await?.json().await?;
 
     let timestamp = derebit_response.result.timestamp.into();
     let mut mark_iv = Decimal::from_f64(derebit_response.result.mark_iv).unwrap();
@@ -52,14 +45,10 @@ async fn main() {
 
     // --- Send the callback to the contract with Switchboard verification ---
     let callback = receiver_contract.callback(mark_iv.mantissa().into(), timestamp);
-    function_runner
-        .emit(
-            receiver,
-            (Utc::now().timestamp() + 120).into(),
-            5_500_000.into(),
-            vec![callback],
-        )
-        .unwrap();
+    let expiration = (Utc::now().timestamp() + 120).into();
+    let gas_limit = 5_500_000.into();
+    function_runner.emit(receiver, expiration, gas_limit, vec![callback])?;
+    Ok(())
 }
 
 /// Run `cargo test -- --nocapture`

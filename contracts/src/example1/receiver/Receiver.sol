2c25c8574e6de154c5000415b449756aae05d653
Albert Hermida <alberthermida@gmail.com>
2023-10-25 23:34:59 -0400

    Fix compilation with new interface

--- contracts/src/example1/receiver/Receiver.sol
+++ contracts/src/example1/receiver/Receiver.sol
@@ -6,24 +6,50 @@ import {AdminLib} from "../admin/AdminLib.sol";
 import {ErrorLib} from "../error/ErrorLib.sol";
 
 // Get the Switchboard Library - this is the Core Mainnet Deployment, you can swap this for one of the networks below
-import {Switchboard} from "@switchboard-xyz/evm.js/contracts/arbitrum/testnet/Switchboard.sol";
+import {SwitchboardCallbackHandler} from "@switchboard-xyz/evm.js/contracts/SwitchboardCallbackHandler.sol";
 
-contract Receiver {
-
-    function callback(int256 data, uint256 timestamp) external {
-        address functionId = Switchboard.getEncodedFunctionId();
+contract Receiver is SwitchboardCallbackHandler {
+    function callback(
+        int256 data,
+        uint256 timestamp
+    ) external isSwitchboardCaller isFunctionId {
+        // Set function id on first callback
+        address functionId = getEncodedFunctionId();
         if (AdminLib.functionId() == address(0)) {
             AdminLib.setFunctionId(functionId);
         }
 
-        // Assert that the sender is switchboard & the correct function id is encoded
-        if (functionId != AdminLib.functionId()) {
-            revert ErrorLib.InvalidSender(AdminLib.functionId(), functionId);
-        }
         ReceiverLib.callback(data, timestamp);
     }
 
     function viewData() external view returns (int256, uint256) {
         return ReceiverLib.viewData();
+    }
+
+    // SwitchboardCallbackHandler functions integrated below
+
+    function getSwithboardAddress() internal view override returns (address) {
+        return AdminLib.switchboard();
+    }
+
+    function getSwitchboardFunctionId()
+        internal
+        view
+        override
+        returns (address)
+    {
+        return AdminLib.functionId();
+    }
+
+    function getEncodedFunctionId() internal pure returns (address) {
+        if (msg.data.length < 20) {
+            revert SwitchboardCallbackHandler__MissingFunctionId();
+        }
+
+        address receivedFunctionId;
+        assembly {
+            receivedFunctionId := shr(96, calldataload(sub(calldatasize(), 20)))
+        }
+        return receivedFunctionId;
     }
 }
